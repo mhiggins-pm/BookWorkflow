e316e3e0872e844f74e2116b1d3374c3e9a39620
mhiggins <mwhiggins@pobox.com>
2021-02-13 22:52:15 +0000

    API definition transferred by SwaggerHub

---
+++ python-flask/swagger_server/controllers/book_controller.py
@@ -0,0 +1,17 @@
+import connexion
+import six
+
+from swagger_server.models.error_model import ErrorModel  # noqa: E501
+from swagger_server.models.inline_response200 import InlineResponse200  # noqa: E501
+from swagger_server import util
+
+
+def a12125():  # noqa: E501
+    """a12125
+
+    List the books # noqa: E501
+
+
+    :rtype: InlineResponse200
+    """
+    return 'do some magic!'
---
+++ python-flask/swagger_server/test/test_book_controller.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from flask import json
+from six import BytesIO
+
+from swagger_server.models.error_model import ErrorModel  # noqa: E501
+from swagger_server.models.inline_response200 import InlineResponse200  # noqa: E501
+from swagger_server.test import BaseTestCase
+
+
+class TestBookController(BaseTestCase):
+    """BookController integration test stubs"""
+
+    def test_a12125(self):
+        """Test case for a12125
+
+        
+        """
+        response = self.client.open(
+            '/virts/SmartBear_Org/Book/1.1.0/book',
+            method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
